b31b524f5221760b716c60f923e2db5d54d858c5
chark1es <charles15851@gmail.com>
2025-11-10 16:06:09 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
     site: "https://hdsi-lab-3-0.github.io",
+    base: "/website",
     integrations: [mdx(), sitemap()],
 });
